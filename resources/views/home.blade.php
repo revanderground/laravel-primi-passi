d0fdca1eed17cd7ad4273bf2432798a24cb4d30f
vtnboolean <vnthuanx@gmail.com>
2022-09-06 14:34:05 +0200

    fine esrcizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,20 @@
+
+<!-- Esercizio di oggi: :laravel: Laravel Primi Passi :laravel:
+nome repo: laravel-primi-passi
+Ciao ragazze e ciao ragazzi!
+Oggi iniziamo a muovere i primi passi con questo fantastico framework che è Laravel!
+
+Descrizione:
+Per prima cosa, creiamo un nuovo progetto Laravel 7,
+utilizzando questo comando: composer create-project --prefer-dist laravel/laravel:^7.0 laravel-primi-passi
+Oppure utilizziamo il medesimo comando nella cartella già creata.
+Al termine dell'installazione, entriamo nella cartella del progetto cd laravel-primi-passi
+e avviamo l'artisan serve con uno di questi due comandi: php artisan serve oppure, se il primo non funziona:  php -S localhost:8000 -t public
+A questo punto, iniziamo a prendere confidenza con le rotte e le views:
+cancelliamo la view welcome.blade.php e creiamo una nostra homepage.
+Facciamo quindi sì che la rotta / visualizzi home.blade.php
+Inizialmente stampiamo un Hello World, poi sentiamoci liberi di sperimentare. -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
