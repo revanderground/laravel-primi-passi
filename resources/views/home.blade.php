8a2d8dc68bbfe8153e3686cb116123c57dfc104c
vtnboolean <vnthuanx@gmail.com>
2022-09-06 14:32:18 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Laravel Primi Passi </title>
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}" >
+
+
+</head>
+<body>
+
+    <div>
+        <h1>Hello World</h1>
+        <a href="/second-page">
+            <h2>Link alla seconda pagina</h2>
+        </a>
+       
+    </div>
+
+    <div class="image-container">
+         <img  class="splash-image" src="https://wips.plug.it/cips/tecnologia/cms/2021/08/mondo.jpg?w=738&a=c&h=415" alt="globe">
+    </div>
+  
+
+
+    
+</body>
+</html>
